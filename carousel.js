5cd975e42839e2f01413a2e92dac3bfdbec0226a
Ertersy40 <wonk4040@gmail.com>
2023-12-30 13:48:08 +1100

    Thinner gaps bigger images

--- carousel.js
+++ carousel.js
@@ -2,24 +2,26 @@ document.addEventListener("DOMContentLoaded", function() {
     const carousel = document.getElementById('carousel');
     const images = carousel.getElementsByTagName('img');
     const imageCount = images.length;
+    const gap = 4;
     let index = 0;
+    
 
     // Clone the first few images and append them to the carousel
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < imageCount; i++) {
         const clone = images[i].cloneNode(true);
         carousel.appendChild(clone);
     }
 
     // Set initial image positions
     for (let i = 0; i < carousel.children.length; i++) {
-        carousel.children[i].style.transform = `translateY(${i * (100 + 20)}%)`;
+        carousel.children[i].style.transform = `translateY(${i * (100 + gap)}%)`;
     }
 
     function nextImage() {
         index++;
         for (let i = 0; i < carousel.children.length; i++) {
             setTimeout(() => {
-                carousel.children[i].style.transform = `translateY(${(i - index) * (100 + 20)}%)`;
+                carousel.children[i].style.transform = `translateY(${(i - index) * (100 + gap)}%)`;
             }, 1500)
         }
     
@@ -27,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
             // Instantly reset the position of the images to the start
             for (let i = 0; i < carousel.children.length; i++) {
                 carousel.children[i].style.transition = 'none'; // Temporarily remove the transition
-                carousel.children[i].style.transform = `translateY(${i * (100 + 20)}%)`;
+                carousel.children[i].style.transform = `translateY(${i * (100 + gap)}%)`;
             }
 
             // Force a reflow
